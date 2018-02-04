569dd8c272e3bb95810ed51d4890b3c335cf9860
Adrien <adrien7703@gmail.com>
2018-02-03 21:02:15 -0500

    Added the initial files

---
+++ SunriseRPG/src/sunrise/main/RPGPlugin.java
@@ -0,0 +1,16 @@
+package sunrise.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RPGPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+	
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+}
